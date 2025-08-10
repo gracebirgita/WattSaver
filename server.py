458b6f4314990216d31a15f354afb3883f213803
Grace Birgitta Hadhinata <grazenata@gmail.com>
2025-08-10 18:52:20 +0700

    add login,register-backend flask

--- server.py
+++ server.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect, flash
 # from waitress import serve
-import libsql_client
+# import libsql_client
+# from libsql_client import create_client
+import libsql
+
+import sqlite3
 
 app = Flask(__name__)
-# koneksi turso
-# conn = libsql_client.connect(
-#     url="libsql://wattsaver-shiroewt.aws-ap-northeast-1.turso.io",
-#     auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NTM5Nzg0NjYsImlkIjoiY2FmOTgzZDQtZTkyZS00MTlkLTgzMjgtYTBhM2UxZTc3ZGRmIiwicmlkIjoiNjhiNGMyODgtZjBhMi00MzYwLTg0MTctNDAzNjE3YmE0YjkyIn0.I0cd3UsLdvC9w8zy7HFONwH9UEqSGZidTE-Sp8z1fFsEWOrkW-CO63qeDZJpc8i5DcoiVPEGrcmrl2puVGvmDg"
-# )
+app.secret_key = 'secret-key'
 
+# turso db shell libsql://wattsaver-shiroewt.aws-ap-northeast-1.turso.io  eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NTM5Nzg0NjYsImlkIjoiY2FmOTgzZDQtZTkyZS00MTlkLTgzMjgtYTBhM2UxZTc3ZGRmIiwicmlkIjoiNjhiNGMyODgtZjBhMi00MzYwLTg0MTctNDAzNjE3YmE0YjkyIn0.I0cd3UsLdvC9w8zy7HFONwH9UEqSGZidTE-Sp8z1fFsEWOrkW-CO63qeDZJpc8i5DcoiVPEGrcmrl2puVGvmDg
+
+def get_db():
+    # koneksi turso
+    conn = libsql.connect(
+        database="libsql://wattsaver-shiroewt.aws-ap-northeast-1.turso.io",
+        auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NTM5Nzg0NjYsImlkIjoiY2FmOTgzZDQtZTkyZS00MTlkLTgzMjgtYTBhM2UxZTc3ZGRmIiwicmlkIjoiNjhiNGMyODgtZjBhMi00MzYwLTg0MTctNDAzNjE3YmE0YjkyIn0.I0cd3UsLdvC9w8zy7HFONwH9UEqSGZidTE-Sp8z1fFsEWOrkW-CO63qeDZJpc8i5DcoiVPEGrcmrl2puVGvmDg"
+    )
+    # conn = sqlite3.connect('WattSaver.db')
+    return conn
+
+def check_user_db():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM User")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    conn.close()
+    
 
 @app.route("/")
 
@@ -17,22 +38,76 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# logged in status
+@app.context_processor
+def inject_logged_in():
+    return dict(logged_in =('username' in session))
+
 # login
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # connect db
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM User WHERE user_name=? AND password=?', (username, password))
+        user = cur.fetchone()
+        conn.close()
+
+        if user:
+            session['username']= username
+            return redirect(url_for('index'))
+        else:
+            flash('nama pengguna  atau password tidak sesuai')
+
     return render_template('login.html')
 
+
 # register
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method=='POST':
+        username = request.form['username'] # name atribut
+        password = request.form['password']
+        email = request.form['email']
+
+        if len(password) < 5:
+            flash("Password minimal 5 karakter")
+            return render_template('register.html')
+        
+        # connect db
+        conn = get_db()
+        cur = conn.cursor()
+        
+        # check duplicate account
+        cur.execute('SELECT * FROM User WHERE user_name=?', (username,))
+        account_exist = cur.fetchone() # row from database
+        if account_exist:
+            conn.close()
+            flash('Akun sudah ada, silahkan login')
+            return render_template('register.html')
+
+        cur.execute('INSERT INTO User(user_name, email, password) VALUES (?,?,?)', (username, email,password))
+        conn.commit()
+        conn.close()
+        check_user_db()
+        flash('Registrasi berhasil! silahkan login')
+        return redirect(url_for('login'))
+
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/analisis.html')
 def analisis():
-    # backend code
-    return render_template(
-        "analisis.html"
-    )
+
+    return render_template("analisis.html")
 
 @app.route('/edit.html')
 def edit():
